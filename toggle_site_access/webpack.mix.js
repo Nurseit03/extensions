e34ec7183946273c59057fdc59ab261b06d99972
Nurseit Bakytbekov <66901719+Nurseit03@users.noreply.github.com>
2024-03-14 17:41:25 +0600

    Delete toggle_site_access directory

--- toggle_site_access/webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('./')
-  .sass('assets/sass/popup.scss', 'dist/css')
-  .js('assets/js/background.js', 'dist/js')
-  .js('assets/js/popup.js', 'dist/js').vue()
-  .options({ processCssUrls: false });
